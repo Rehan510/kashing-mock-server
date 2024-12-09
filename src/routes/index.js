ef20ad78c2a238f8f135ab178648dd4778d33105
Rehan510 <rehan51055@gmail.com>
2024-12-09 20:26:57 +0500

    removed unused file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import userManagement from "./user.js";
 import wallets from "./wallets.js";
 const router = express.Router();
 const defaultRoutes = [
-  { path: "/", route: userManagement, isActive: true },
   { path: "/wallets", route: wallets, isActive: true },
 ];
 
